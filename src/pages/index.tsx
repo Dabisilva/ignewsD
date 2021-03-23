f798a833c024d47a9e4a745d61b5d35b318935b2
Davi <dabisilvaond@gmail.com>
2021-03-23 09:55:26 -0300

    criando componente header, SignInButton e tela home com toda estilização

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
