eaadbdc93594576569fb2a0a3b9ec57d49c1823c
Davi <dabisilvaond@gmail.com>
2021-03-23 15:32:16 -0300

    signIn de usuário com github, e salvando usuário no banco de dados com faunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-
+import { signIn, signOut, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
-  return isUserLoggedIn ? (
-    <button className={styles.buttonContainer} type="button">
+  const [session] = useSession();
+
+  return session ? (
+    <button
+      className={styles.buttonContainer}
+      type="button"
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Davi Barbosa
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.buttonContainer} type="button">
+    <button
+      className={styles.buttonContainer}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
