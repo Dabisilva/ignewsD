da76c6be96754f73abe845148403676b3fe8ccf6
Davi <dabisilvaond@gmail.com>
2021-08-17 14:22:41 -0300

    fixing bugs in subscribeButton

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -54,7 +54,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   const { slug } = params;
 
-  if (!session?.activeSubscription) {
+  if (!session.activeSubscription) {
     return {
       redirect: {
         destination: `/posts/preview/${slug}`,
